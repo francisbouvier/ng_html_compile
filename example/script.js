14995f6c253766f8b5b65a62f8cfc11219a5d81d
Francis Bouvier <francis.bouvier@xerus-technologies.fr>
2013-08-05 18:21:16 +0200

    Example: string quote correction

--- example/script.js
+++ example/script.js
@@ -5,9 +5,9 @@ function HomeCtrl($scope, $compile) {
     $scope.firstName = 'Bruce';
     $scope.lastName = 'Willis';
 
-    $scope.template = "First name : <input type='text' ng-model='firstName' />";
+    $scope.template = 'First name : <input type="text" ng-model="firstName" />';
 
     $scope.change = function() {
-	$scope.template = "Last Name : <input type='text' ng-model='lastName' />";
+	$scope.template = 'Last Name : <input type="text" ng-model="lastName" />';
     };
 };
